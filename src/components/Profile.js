e584aa0fec031b42c6299fbfa80c0812f3684edf
GTolan <tolangerard@gmail.com>
2021-10-05 13:03:41 +0200

    start profile and create chat

---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const Profile = () => {
+    return (
+        <main>
+            <section className="menu">
+                <ul>
+                    <li><Link to="/meeting-room"></Link></li>
+                </ul>
+            </section>
+            <section className="menu">
+                
+            </section>
+            
+        </main>
+    )
+}
+
+export default Profile
